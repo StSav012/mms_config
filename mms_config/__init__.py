e1e4a585e424e4a5730f634fc538e51792c9148c
StSav012 <stsav012@gmail.com>
2024-06-02 09:49:24 +0300

    Search for translations relatively to the current file

--- mms_config/__init__.py
+++ mms_config/__init__.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 def main() -> int:
     import sys
+    from pathlib import Path
 
     from packaging.version import parse
     from qtpy import QT_VERSION
@@ -15,19 +16,17 @@ def main() -> int:
 
     languages: set[str] = set(QLocale().uiLanguages() + [QLocale().bcp47Name(), QLocale().name()])
     language: str
+    translations_path: str = QLibraryInfo.path(QLibraryInfo.LibraryPath.TranslationsPath)
     qtbase_translator: QTranslator = QTranslator()
     for language in languages:
-        if qtbase_translator.load(
-                'qtbase_' + language,
-                QLibraryInfo.location(QLibraryInfo.LibraryPath.TranslationsPath),
-        ):
-            if QApplication.installTranslator(qtbase_translator):
-                break
+        if qtbase_translator.load("qtbase_" + language, translations_path):
+            QApplication.installTranslator(qtbase_translator)
+            break
     my_translator: QTranslator = QTranslator()
     for language in languages:
-        if my_translator.load(language, 'i18n'):
-            if QApplication.installTranslator(my_translator):
-                break
+        if my_translator.load(language, str(Path(__file__).parent / "i18n")):
+            QApplication.installTranslator(my_translator)
+            break
     w: FormMain = FormMain()
     w.show()
     return app.exec()
